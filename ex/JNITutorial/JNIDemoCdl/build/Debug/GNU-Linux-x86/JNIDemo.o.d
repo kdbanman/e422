a26c768c417779fc4ece9e2e3e47f886206759f0
kdbanman <kdbanman@ualberta.ca>
2014-10-20 01:41:16 -0600

    maintain files

--- ex/JNITutorial/JNIDemoCdl/build/Debug/GNU-Linux-x86/JNIDemo.o.d
+++
@@ -1,9 +0,0 @@
-build/Debug/GNU-Linux-x86/JNIDemo.o: JNIDemo.c JNIDemoJava.h \
-  /home/smorgan/jdk1.6.0_14/include/jni.h \
-  /home/smorgan/jdk1.6.0_14/include/linux/jni_md.h
-
-JNIDemoJava.h:
-
-/home/smorgan/jdk1.6.0_14/include/jni.h:
-
-/home/smorgan/jdk1.6.0_14/include/linux/jni_md.h:
