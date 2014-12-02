78b6e2b5bbce4b1ba977dd94dfcc015d22b5835b
kdbanman <kdbanman@ualberta.ca>
2014-12-01 20:53:52 -0700

    put in example

--- ass2/EncClient/src/encclient/EncClient.java
+++ ass2/EncClient/src/encclient/EncClient.java
@@ -1,16 +1,44 @@
 package encclient;
 
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
 /**
  *
  * @author kdbanman
  */
 public class EncClient {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        
-    }
+    public static void main(String [] args)
+   {
+      String serverName = args[0];
+      int port = Integer.parseInt(args[1]);
+      try
+      {
+         System.out.println("Connecting to " + serverName
+                             + " on port " + port);
+         Socket client = new Socket(serverName, port);
+         System.out.println("Just connected to "
+                      + client.getRemoteSocketAddress());
+         OutputStream outToServer = client.getOutputStream();
+         DataOutputStream out =
+                       new DataOutputStream(outToServer);
+
+         out.writeUTF("Hello from "
+                      + client.getLocalSocketAddress());
+         InputStream inFromServer = client.getInputStream();
+         DataInputStream in =
+                        new DataInputStream(inFromServer);
+         System.out.println("Server says " + in.readUTF());
+         client.close();
+      }catch(IOException e)
+      {
+         e.printStackTrace();
+      }
+   }
     
 }
